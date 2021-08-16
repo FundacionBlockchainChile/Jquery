c72ae11da8a512dfa1a928abadda267134467e0c
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2021-08-16 18:54:21 -0400

    Delete Message added

--- Ninjas/js/scripts.js
+++ Ninjas/js/scripts.js
@@ -4,14 +4,23 @@ $(function () {
   let $deleteBtn = $('#delete')
   let $restoreBtn = $('#restore')
 
+  $('h5').hide()
+
   $card.click(function () {
-    $(this).hide()
+    $(this).hide(1000)
   })
+  $card
+    .mouseenter(function () {
+      $(this).children('h5').show()
+    })
+    .mouseleave(function () {
+      $(this).children('h5').hide()
+    })
 
   $deleteBtn.click(function () {
-    $('.card').hide()
+    $('.card').hide(1000)
   })
   $restoreBtn.click(function () {
-    $('.card').show()
+    $('.card').show(1000)
   })
 })
