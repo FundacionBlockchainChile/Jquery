e541a6a0478d1a9b46388020c70efa161e9c8601
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2021-08-22 19:20:30 -0400

    Pokemons Gallery with AJAX fetch added

--- Pokemones/js/scripts.js
+++ Pokemones/js/scripts.js
@@ -5,15 +5,43 @@ $(function () {
   let $loadBtn = $('#loadBtn')
   let $i = 1
 
-  const addDeleteMessage = () => {
-    let $card = $('.card')
+  const renderSkills = (res) => {
+    $('#name').text(res.name.charAt(0).toUpperCase() + res.name.slice(1))
+    $('#typeOfPokemon').text(
+      res.types[0].type.name.charAt(0).toUpperCase() +
+        res.types[0].type.name.slice(1)
+    )
+    $('#height').text(res.height)
+    $('#weight').text(res.weight)
+  }
+
+  const clearSkills = () => {
+    $('#name').text('')
+    $('#typeOfPokemon').text('')
+    $('#height').text('')
+    $('#weight').text('')
+  }
+
+  const addDeletAndHover = (id) => {
+    let $card = $(`#${id}`)
+
     $card.click(function () {
       $(this).hide(1000)
+      clearSkills()
     })
+
     $card
       .mouseenter(function () {
         $(this).removeClass('bg-dark')
         $(this).addClass('bg-danger')
+        let id = $(this).attr('id')
+        console.log(id)
+        $.get(
+          `https://pokeapi.co/api/v2/pokemon/${id}`,
+          function showPokemonSkills(res) {
+            renderSkills(res)
+          }
+        )
       })
       .mouseleave(function () {
         $(this).removeClass('bg-danger')
@@ -23,9 +51,9 @@ $(function () {
 
   const loadPokemones = () => {
     $pokemonContainer.append(
-      `<div class="card bg-dark" style="width: 5rem"><img class="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${$i}.png" alt="Card image cap" /></div>`
+      `<div id="${$i}" class="card bg-dark" style="width: 6.5rem"><img class="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${$i}.png" alt="Card image cap" /></div>`
     )
-    addDeleteMessage()
+    addDeletAndHover($i)
     $i++
   }
 
@@ -33,13 +61,16 @@ $(function () {
 
   $deleteBtn.click(function () {
     $('.card').hide(1000)
+    clearSkills()
   })
+
   $loadBtn.click(function () {
     for (let j = 0; j < 15; j++) {
       loadPokemones()
     }
   })
 
+  // Load first 15 pokemones at Init
   for (let j = 0; j < 15; j++) {
     loadPokemones()
   }
